c9b80d9a15392ed462733e3974d7409a6f3197e1
Animesh Choudhury <94227992+Darkdodo004@users.noreply.github.com>
2024-08-21 14:25:45 +0530

    Delete task2/src/components directory

--- task2/src/components/FilterBar.js
+++
@@ -1,9 +0,0 @@
-export const fetchProductById = async (id) => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/products/${id}`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching product by ID", error);
-        return null;
-    }
-};
